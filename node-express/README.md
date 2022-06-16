ebc98a9cb24cfe4273f66073ec9d663c23099faf
Omar Ceja <oceja@users.noreply.github.com>
2022-06-15 20:45:50 -0700

    Implementations in rust/go, clean up READMEs + clean up python/node

--- node-express/README.md
+++ node-express/README.md
@@ -1,4 +1,4 @@
-# Example Alchemy Notify Webhooks Server
+# Example Alchemy Notify Webhooks Server in Node
 
 A simple example webhook server for using Alchemy Notify that uses node express.
 
@@ -20,24 +20,18 @@ yarn
 
 ## Run
 
-To run on port 80:
+To run on localhost:8080:
 
 ```
-yarn start 80
+PORT=8080 HOST=localhost SIGNING_KEY=whsec_your_key_here yarn start
 ```
+
+Please change SIGNING_KEY to the signing key corresponding to your webhook, which you can find [here](https://docs.alchemy.com/alchemy/enhanced-apis/notify-api/using-notify#1.-find-your-signing-key)
 
 And just like that, you're done!
 
-Note: there are at least two hard coded variables in that you will have to modify to fit your needs:
+NOTE: Your webhook path is currently set to "/webhook-path" in `src/index.ts`, but feel free to change it to whatever path you'd like.
 
-- `signingKey`: your webhook signing key, currently set to "whsec_test" in `index.ts`, please change it your actual signing key which you can find [here](https://docs.alchemy.com/alchemy/enhanced-apis/notify-api/using-notify#1.-find-your-signing-key)
+## Debugging
 
-- `path`: your webhook path, currently set to "/webhook-path" in `index.ts`, but feel free to change it to whatever path you'd like
-
-## Test Locally
-
-To test your webhook locally, we recommend using ngrok and following the guide [here](https://docs.alchemy.com/alchemy/enhanced-apis/notify-api/using-notify#1.-find-your-signing-key). NOTE: When you fire up your local forwarding tunnel be sure to use the same port as the server above.
-
-Example on port 80:
-
-`ngrok http 80`
+If you aren't receiving any webhooks, be sure you followed the steps [here first](https://github.com/alchemyplatform/#readme).
--- node-express/src/index.ts
+++ node-express/src/index.ts
@@ -1,15 +1,21 @@
 import express from "express";
+import { getRequiredEnvVar, setDefaultEnvVar } from "./envHelpers";
 import {
   addAlchemyContextToRequest,
   validateAlchemySignature,
-  WebhookEvent,
+  AlchemyWebhookEvent,
 } from "./webhooksUtil";
 
 async function main(): Promise<void> {
   const app = express();
-  const port = process.argv[2] ?? 80;
-  // TODO: update to your own webhook signing key (which you can find in your dashboard)
-  const signingKey = "whsec_test";
+
+  setDefaultEnvVar("PORT", "8080");
+  setDefaultEnvVar("HOST", "127.0.0.1");
+  setDefaultEnvVar("SIGNING_KEY", "whsec_test");
+
+  const port = +getRequiredEnvVar("PORT");
+  const host = getRequiredEnvVar("HOST");
+  const signingKey = getRequiredEnvVar("SIGNING_KEY");
 
   // Middleware needed to validate the alchemy signature
   app.use(
@@ -19,17 +25,19 @@ async function main(): Promise<void> {
   );
   app.use(validateAlchemySignature(signingKey));
 
+  // Register handler for Alchemy Notify webhook events
   // TODO: update to your own webhook path
   app.post("/webhook-path", (req, res) => {
-    const webhookEvent = req.body as WebhookEvent;
-    // Do stuff with with webhook event here! Be sure to respond with 200
-    console.log(webhookEvent);
+    const webhookEvent = req.body as AlchemyWebhookEvent;
+    // Do stuff with with webhook event here!
+    console.log(`Processing webhook event id: ${webhookEvent.id}`);
     // Be sure to respond with 200 when you successfully process the event
     res.send("Alchemy Notify is the best!");
   });
 
-  app.listen(port, () => {
-    console.log(`Example Alchemy Notify app listening on port ${port}`);
+  // Listen to Alchemy Notify webhook events
+  app.listen(port, host, () => {
+    console.log(`Example Alchemy Notify app listening at ${host}:${port}`);
   });
 }
 
